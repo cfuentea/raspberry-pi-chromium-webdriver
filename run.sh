d8766be809aa340f421f277ecc7633f280efd27d
Carlos Fuentealba <carlos@fuentealba.dev>
2023-08-20 20:39:36 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm -w /tmp -v $(pwd):/tmp -h $(hostname) cfuentealba/chromium-arm64:latest python3 test.py
+docker run --rm -w /tmp -v /home/soporte/raspberry-pi-chromium-webdriver:/tmp -h $(hostname) cfuentealba/chromium-arm64:latest python3 test.py
