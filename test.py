992b0fcd25d7f16474a2cba6f555767068379be1
Carlos Fuentealba <carlos@fuentealba.dev>
2023-08-20 20:05:37 -0400

    Update test.py

--- test.py
+++ test.py
@@ -48,6 +48,7 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--mute-audio')
 #chrome_options.add_argument('--disable-dev-shm-usage')
 
 # version script ejecutado en ontenedor
